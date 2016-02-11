de5f10ca95ba42e1ad856046339c2477c9e1455e
Quietess <rofleclipse@gmail.com>
2016-02-11 04:45:45 -0500

    fixed

--- public/redirect.php
+++ public/redirect.php
@@ -3,8 +3,8 @@ $data = json_decode(file_get_contents('http://a.4cdn.org/vg/catalog.json'), true
  
 foreach((array)$data as $result) {
     foreach ($result['threads'] as $thread){
-        if (strpos($thread['sub'], 'RuneScape General') !== false) { //you can change RuneScape General to whatever your general usually has in the subject field
-            echo '<meta HTTP-EQUIV="REFRESH" content="0; url=http://boards.4chan.org/vg/thread/' . $thread['no'] . '">'; //make sure to change /rsg/ to whatever it is
+        if (strpos($thread['sub'], 'RuneScape General') !== false) { 
+            echo '<meta http-equiv="refresh" content="0; url=http://boards.4chan.org/vg/thread/' . $thread['no'] . '">';
             }
         }
     }
